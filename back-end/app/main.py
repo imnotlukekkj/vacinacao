21ee3c0654aefd1eb3926b4175e48b91ba7727e9
Luis Felipe Leandro Gomes <felipegomeslflg42@gmail.com>
2025-11-01 00:02:00 -0300

    fix: Adiciona a URL do Vercel

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -13,6 +13,7 @@ app = FastAPI(title="Vacina Brasil API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://vacinacao-delta.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:8081",
